03cc56b856f7a067af12e4fac39cc096332c6d68
Juliya Strugatskaya <strugatskaya.um@gmail.com>
2021-05-20 21:19:57 +0300

    .java added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 35_870;
+        int oneMileCost = 20;
+        double totalMilesBonuses = (double)ticketPrice / oneMileCost;
+        System.out.println(totalMilesBonuses);
+    }
+}
+
